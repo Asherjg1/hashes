a28a0e43c6877645ad9b7e85b67f4a2edb6c7334
Asherjg1 <asherjg1@gmail.com>
2019-06-16 13:17:50 -0700

    too much hypebeast

--- hypebeast.js
+++ hypebeast.js
@@ -1,3 +1,14 @@
-var hypebeast =["yeezys","Air Jordan 1 high","Air Jordan 11","sean witherspoon air max 97","ultraboosts"]
-var myString="air max 90"
-var myHash {color:"university red",name: "air max 90 custom",price:140}
+var hypebeast =["yeezys","Air Jordan 1 high","Air Jordan 11","sean witherspoon air max 97","ultraboosts",]
+var myString="air max 90. "
+var myHash = {color:"university red",name: "air max 90 custom",price:140}
+hypebeast.push("nike air force 1")
+ myString += "the shoes that are always sold out"
+ myHash["year"]=95
+hypebeast.pop()
+myString = myString.split('the shoes that are always sold out').join('')
+delete myHash['year']
+
+
+ console.log(hypebeast);
+ console.log(myString);
+ console.log(myHash);
