057659581e88bafc8b190563e6db0d898805ee9f
Asherjg1 <asherjg1@gmail.com>
2019-06-16 13:28:23 -0700

    too much hypebeast

--- hypebeast.js
+++ hypebeast.js
@@ -1,14 +1,17 @@
 var hypebeast =["yeezys","Air Jordan 1 high","Air Jordan 11","sean witherspoon air max 97","ultraboosts",]
 var myString="air max 90. "
 var myHash = {color:"university red",name: "air max 90 custom",price:140}
+var myHash2 = {designer:'nike',hypebeastLevel:10,numMade:10}
 hypebeast.push("nike air force 1")
  myString += "the shoes that are always sold out"
  myHash["year"]=95
 hypebeast.pop()
 myString = myString.split('the shoes that are always sold out').join('')
 delete myHash['year']
-
+hypebeast+=hypebeast
+myString+=myString
+var merged = {...myHash,...myHash2}
 
  console.log(hypebeast);
  console.log(myString);
- console.log(myHash);
+ console.log(merged);
